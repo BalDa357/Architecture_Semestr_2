8c58375b7c3a6bb016220c7d0fb5d9857ff63cf8
Vitalii Viflinzider <megobonus666@gmail.com>
2016-11-09 19:36:46 +0200

    Add parser

--- src/lab1/main.go
+++ src/lab1/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func main(){
-    
+func isCorrect(str string) bool {
+	var length int = len(str)
+	var symbols_count int = 0
+	var err error
+
+	for i := 0; i < length; i++ {
+		if string([]rune(str)[i]) == "+" || string([]rune(str)[i]) == "-" {
+			symbols_count++
+			if symbols_count > 1 || i == 0 || i == length {
+				return false
+			}
+		} else {
+			_, err = strconv.Atoi(string([]rune(str)[i]))
+			if err != nil {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Print("Enter expression: ")
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println(isCorrect(input))
 }
