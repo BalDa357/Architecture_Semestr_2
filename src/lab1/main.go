d6c5e3facaa079561f989643680e7dd3526643ba
Vitalii Viflinzider <megobonus666@gmail.com>
2016-11-10 13:13:34 +0200

    Add splitter

--- src/lab1/main.go
+++ src/lab1/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func isCorrect(str string) bool {
@@ -18,17 +19,42 @@ func isCorrect(str string) bool {
 			}
 		} else {
 			_, err = strconv.Atoi(string([]rune(str)[i]))
+
 			if err != nil {
 				return false
 			}
 		}
 	}
+	if symbols_count == 0 {
+		return false
+	}
 	return true
 }
 
+func get_numbers(str string) (string, string, string) {
+	var num []string
+
+	if strings.Contains(str, "+") {
+		num = strings.Split(str, "+")
+		fmt.Println(num)
+		return num[0], num[1], "+"
+	} else {
+		num = strings.Split(str, "-")
+		return num[0], num[1], "-"
+	}
+
+}
+
 func main() {
-	fmt.Print("Enter expression: ")
-	var input string
+	var input, num1, num2, symbol string
+
+	fmt.Println("Enter expression: ")
 	fmt.Scanln(&input)
-	fmt.Println(isCorrect(input))
+
+	if isCorrect(input) {
+		num1, num2, symbol = get_numbers(input)
+
+	} else {
+		fmt.Println("Expression is not correct.")
+	}
 }
